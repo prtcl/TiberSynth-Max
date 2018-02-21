6f00d08ea5ea3951545939a67553c94971d1e230
Cory O'Brien <cory@prtcl.cc>
2018-02-20 21:29:11 -0500

    create store, refactor space

--- server/lib/createStore.js
+++ server/lib/createStore.js
@@ -1,21 +1,22 @@
-const { EventEmitter } = require('events');
 
 const createStore = (actions, state) => {
-  const events = new EventEmitter();
-  const update = () => events.emit('update', state);
-  const dispatch = (action, args) => {
-    const res = action(state, args);
-    update();
-    return res;
+  const subscribers = [];
+  const update = () => {
+    subscribers.forEach((fn) => fn(state));
   };
+  const dispatch = (action, args) => {
+    action(state, args);
+    update();
+  };
+
   return {
     ...Object.keys(actions)
       .reduce((res, name) => ({
         ...res,
         [name]: (args) => dispatch(actions[name], args)
       }), {}),
-    subscribe (listener) {
-      events.on('update', listener);
+    subscribe (fn) {
+      subscribers.push(fn);
     }
   };
 };
